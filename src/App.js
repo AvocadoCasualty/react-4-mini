bd404e7711d61a8d139becec135bde185c787993
AvocadoCasualty <baibeebambi@gmail.com>
2020-05-28 15:33:20 -0700

    Mini notes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import {HashRouter as Router} from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
 import "./App.css";
+import router from "./router";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Nav />
-        <h1 style={{ padding: "200px 35%" }}>
-          This is where your pages will appear
-        </h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Nav/>
+                    {router}
+                    <h1 style={{padding: "200px 35%"}}>
+                        This is where your pages will appear
+                    </h1>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
